6a8a1ad635f720db6f765472830e2ada8a581c59
Rajneesh Rai <rajneeshrai83@yahoo.in>
2024-12-01 14:24:56 +0530

    update: use of http verb GET and path.startswithsegment features

--- Program.cs
+++ Program.cs
@@ -3,24 +3,38 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.Run(async (HttpContext httpContext) => {
-    await httpContext.Response.WriteAsync($"Request method : {httpContext.Request.Method}\r\n");
+app.Run(async (HttpContext httpContext) =>
+{
 
-    await httpContext.Response.WriteAsync($"\r\nQueries\r\n");
+    if (httpContext.Request.Method == HttpMethod.Get.ToString())
+    {
+        if (httpContext.Request.Path.StartsWithSegments("/"))
+        {
+            await httpContext.Response.WriteAsync($"Request method : {httpContext.Request.Method}\r\n");
 
-    foreach (var query in httpContext.Request.Query){
-        await httpContext.Response.WriteAsync($"{query.Key} : {query.Value}\r\n");
-    }
+            await httpContext.Response.WriteAsync($"\r\nQueries\r\n");
 
-    await httpContext.Response.WriteAsync($"\r\nHeaders\r\n");
+            foreach (var query in httpContext.Request.Query)
+            {
+                await httpContext.Response.WriteAsync($"{query.Key} : {query.Value}\r\n");
+            }
 
-    foreach (var header in httpContext.Request.Headers){
-        await httpContext.Response.WriteAsync($"{header.Key} : {header.Value}\r\n");
-    }
+            await httpContext.Response.WriteAsync($"\r\nHeaders\r\n");
 
-    await httpContext.Response.WriteAsync($"\r\nCookies\r\n");
-    foreach(var cookie in httpContext.Request.Cookies){
-        await httpContext.Response.WriteAsync($"{cookie.Key} : {cookie.Value}\r\n");
+            foreach (var header in httpContext.Request.Headers)
+            {
+                await httpContext.Response.WriteAsync($"{header.Key} : {header.Value}\r\n");
+            }
+
+            await httpContext.Response.WriteAsync($"\r\nCookies\r\n");
+            foreach (var cookie in httpContext.Request.Cookies)
+            {
+                await httpContext.Response.WriteAsync($"{cookie.Key} : {cookie.Value}\r\n");
+            }
+        }
+        else if(httpContext.Request.Path.StartsWithSegments("/employee")){
+            await httpContext.Response.WriteAsync("Employees");
+        }
     }
 });
 
