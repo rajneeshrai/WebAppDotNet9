bd1976de26bd14c7e769c9d54cf2cb30c19cf2b6
Rajneesh Rai <rajneeshrai83@yahoo.in>
2024-12-01 15:38:28 +0530

    update: http POST verb learned

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -32,10 +34,55 @@ app.Run(async (HttpContext httpContext) =>
                 await httpContext.Response.WriteAsync($"{cookie.Key} : {cookie.Value}\r\n");
             }
         }
-        else if(httpContext.Request.Path.StartsWithSegments("/employee")){
-            await httpContext.Response.WriteAsync("Employees");
+        else if (httpContext.Request.Path.StartsWithSegments("/employees"))
+        {
+            await httpContext.Response.WriteAsync("Employees\r\n");
+            EmployeeRepository.GetEmployees.ForEach(async (employee) =>
+            {
+                await httpContext.Response.WriteAsync($"Id: {employee.Id},Name: {employee.Name}, Position: {employee.Position}, Salary: {employee.Salary}\r\n");
+            });
+        }
+    }
+    else if (httpContext.Request.Method == HttpMethod.Post.ToString())
+    {
+        if (httpContext.Request.Path.StartsWithSegments("/employees"))
+        {
+            StreamReader reader = new(httpContext.Request.Body);
+            var employee = JsonSerializer.Deserialize<Employee>(await reader.ReadToEndAsync());
+            EmployeeRepository.AddEmployee(employee);
+            await httpContext.Response.WriteAsync("Employee Added");
         }
     }
 });
 
 app.Run();
+
+static class EmployeeRepository
+{
+    private static List<Employee> _employees = [
+            new Employee(1,"Rajneesh", "Tech Lead",1000),
+            new Employee(2,"Rajat", "Manager",1500),
+            new Employee(3,"Rajneesh", "Tech Lead",1000),
+        ];
+    public static List<Employee> GetEmployees => _employees;
+
+    public static void AddEmployee(Employee? employee){
+        if(employee is not null)
+            _employees.Add(employee);
+    }
+}
+
+public class Employee
+{
+    public Employee(int id, string name, string position, decimal salary)
+    {
+        this.Id = id;
+        this.Name = name;
+        this.Position = position;
+        this.Salary = salary;
+    }
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Position { get; set; }
+    public decimal Salary { get; set; }
+}
