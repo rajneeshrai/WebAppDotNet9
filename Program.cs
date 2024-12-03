bf7b911e7ccb0bfeb87e07bed88e4cd7d67784e5
Rajneesh Rai <Rajneesh.R.Rai@shell.com>
2024-12-03 19:29:31 +0530

    update: used app.use to add middleware

--- Program.cs
+++ Program.cs
@@ -3,57 +3,68 @@ using System.Text.Json;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
-app.Run(async (HttpContext httpContext) =>
-{
-
-    if (httpContext.Request.Method == HttpMethod.Get.ToString())
-    {
-        if (httpContext.Request.Path.StartsWithSegments("/"))
-        {
-            await httpContext.Response.WriteAsync($"Request method : {httpContext.Request.Method}\r\n");
-
-            await httpContext.Response.WriteAsync($"\r\nQueries\r\n");
-
-            foreach (var query in httpContext.Request.Query)
-            {
-                await httpContext.Response.WriteAsync($"{query.Key} : {query.Value}\r\n");
-            }
-
-            await httpContext.Response.WriteAsync($"\r\nHeaders\r\n");
-
-            foreach (var header in httpContext.Request.Headers)
-            {
-                await httpContext.Response.WriteAsync($"{header.Key} : {header.Value}\r\n");
-            }
-
-            await httpContext.Response.WriteAsync($"\r\nCookies\r\n");
-            foreach (var cookie in httpContext.Request.Cookies)
-            {
-                await httpContext.Response.WriteAsync($"{cookie.Key} : {cookie.Value}\r\n");
-            }
-        }
-        else if (httpContext.Request.Path.StartsWithSegments("/employees"))
-        {
-            await httpContext.Response.WriteAsync("Employees\r\n");
-            EmployeeRepository.GetEmployees.ForEach(async (employee) =>
-            {
-                await httpContext.Response.WriteAsync($"Id: {employee.Id},Name: {employee.Name}, Position: {employee.Position}, Salary: {employee.Salary}\r\n");
-            });
-        }
-    }
-    else if (httpContext.Request.Method == HttpMethod.Post.ToString())
-    {
-        if (httpContext.Request.Path.StartsWithSegments("/employees"))
-        {
-            StreamReader reader = new(httpContext.Request.Body);
-            var employee = JsonSerializer.Deserialize<Employee>(await reader.ReadToEndAsync());
-            EmployeeRepository.AddEmployee(employee);
-            await httpContext.Response.WriteAsync("Employee Added");
-        }
-    }
+// app.MapGet("/", () => "Hello World!");
+app.Use(async (HttpContext context, RequestDelegate next) => {
+    await context.Response.WriteAsync("Before first middleware execution!\r\n");
+    await next(context);
+    await context.Response.WriteAsync("After first middleware execution!\r\n");
 });
+
+app.Use(async (context, next) => {
+    await context.Response.WriteAsync("Before second middleware execution!\r\n");
+    await next(context);
+    await context.Response.WriteAsync("After second middleware execution!\r\n");
+});
+
+// app.Run(async (HttpContext httpContext) =>
+// {
+
+//     if (httpContext.Request.Method == HttpMethod.Get.ToString())
+//     {
+//         if (httpContext.Request.Path.StartsWithSegments("/"))
+//         {
+//             await httpContext.Response.WriteAsync($"Request method : {httpContext.Request.Method}\r\n");
+
+//             await httpContext.Response.WriteAsync($"\r\nQueries\r\n");
+
+//             foreach (var query in httpContext.Request.Query)
+//             {
+//                 await httpContext.Response.WriteAsync($"{query.Key} : {query.Value}\r\n");
+//             }
+
+//             await httpContext.Response.WriteAsync($"\r\nHeaders\r\n");
+
+//             foreach (var header in httpContext.Request.Headers)
+//             {
+//                 await httpContext.Response.WriteAsync($"{header.Key} : {header.Value}\r\n");
+//             }
+
+//             await httpContext.Response.WriteAsync($"\r\nCookies\r\n");
+//             foreach (var cookie in httpContext.Request.Cookies)
+//             {
+//                 await httpContext.Response.WriteAsync($"{cookie.Key} : {cookie.Value}\r\n");
+//             }
+//         }
+//         else if (httpContext.Request.Path.StartsWithSegments("/employees"))
+//         {
+//             await httpContext.Response.WriteAsync("Employees\r\n");
+//             EmployeeRepository.GetEmployees.ForEach(async (employee) =>
+//             {
+//                 await httpContext.Response.WriteAsync($"Id: {employee.Id},Name: {employee.Name}, Position: {employee.Position}, Salary: {employee.Salary}\r\n");
+//             });
+//         }
+//     }
+//     else if (httpContext.Request.Method == HttpMethod.Post.ToString())
+//     {
+//         if (httpContext.Request.Path.StartsWithSegments("/employees"))
+//         {
+//             StreamReader reader = new(httpContext.Request.Body);
+//             var employee = JsonSerializer.Deserialize<Employee>(await reader.ReadToEndAsync());
+//             EmployeeRepository.AddEmployee(employee);
+//             await httpContext.Response.WriteAsync("Employee Added");
+//         }
+//     }
+// });
 
 app.Run();
 
